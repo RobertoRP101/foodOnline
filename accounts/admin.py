d19874b89515f00bf7d8982c69813be716c71db3
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-14 03:15:28 -0600

    No editable password and user display fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email', 'role', 'is_active')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
