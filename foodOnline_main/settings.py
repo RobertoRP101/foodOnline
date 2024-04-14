3d2738f5d624d811a6bc6e34c0b5ccd536b681d8
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-14 04:24:50 -0600

    Media files configuration

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = [
     'foodOnline_main/static',
 ]
 
+# Media file configurations
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
