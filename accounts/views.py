1b611f71ba96b4c6a37ee999358762103add61bf
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-19 13:16:04 -0600

    Login page setup

--- accounts/views.py
+++ accounts/views.py
@@ -70,5 +70,43 @@ def registerVendor(request):
         'form': form,
         'v_form': v_form
     }
-    
     return render(request, 'accounts/registerVendor.html', context)
+
+def login(request):
+    # if request.method == 'POST':
+    #     form = UserForm(request.POST)
+    #     v_form = VendorForm(request.POST, request.FILES)
+    #     if form.is_valid() and v_form.is_valid():
+    #         first_name = form.cleaned_data['first_name']
+    #         last_name = form.cleaned_data['last_name']
+    #         username = form.cleaned_data['username']
+    #         email = form.cleaned_data['email']
+    #         password = form.cleaned_data['password']
+    #         user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+    #         user.role = user.VENDOR
+    #         user.save()
+    #         vendor = v_form.save(commit=False)
+    #         vendor.user = user
+    #         user_profile = UserProfile.objects.get(user=user)
+    #         vendor.user_profile = user_profile
+    #         vendor.save()
+    #         messages.success(request, 'Your account has been created sucessfully! Please wait for the approval.')
+    #         return redirect('registerVendor')
+    #     else:
+    #         print('invalid form')
+    #         print(form.errors)
+    # else:
+    #     pass
+    # context = {
+    #     'form': form,
+    #     'v_form': v_form
+    # }
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
