925a9c82c6eab0c0494fb2e93f6b0c25ec5c5229
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-19 23:17:08 -0600

    Email configuration

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -144,3 +144,10 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'success',
     messages.INFO: 'info',
 }
+
+# Email configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
