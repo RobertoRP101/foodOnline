e6860777361a47b9d4543fafc1d1b8be20b3a02a
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-20 23:24:43 -0600

    Activating the user

--- accounts/utils.py
+++ accounts/utils.py
@@ -5,6 +5,8 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from foodOnline_main import settings
+
 def detectUser(user):
     if user.role == 1:
         redirectUrl = 'vendorDashboard'
@@ -17,6 +19,7 @@ def detectUser(user):
         return redirectUrl
     
 def send_verification_email(request, user):
+    from_email = settings.DEFAULT_FROM_EMAIL
     current_site = get_current_site(request)
     mail_subject = 'Please activate your account'
     message = render_to_string('accounts/emails/account_verification_email.html', {
@@ -26,5 +29,5 @@ def send_verification_email(request, user):
         'token' : default_token_generator.make_token(user),
     })
     to_mail = user.email
-    mail = EmailMessage(mail_subject, message, to=[to_mail])
+    mail = EmailMessage(mail_subject, message, from_email ,to=[to_mail])
     mail.send()
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-
 from accounts.utils import detectUser, send_verification_email
 from .forms import UserForm
 from .models import User, UserProfile
@@ -8,6 +7,8 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from vendor.forms import VendorForm
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -99,7 +100,20 @@ def registerVendor(request):
 
 def activate(request, uidb64, token):
     # Activate the user by settings the is_activate status to True
-    return 
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulation! Your account is activated.')
+        return redirect('myAccount')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('myAccount')
 
 def login(request):
     if request.user.is_authenticated:
