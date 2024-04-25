0c13abe42939fdb0fa348caf6ec0c4182a59055f
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-24 23:41:15 -0600

    Readonly Latitude/Longitude

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,6 +23,14 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     profile_cover = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     class Meta:
         model = UserProfile
         fields = ['profile_cover', 'cover_photo', 'address_line1', 'address_line2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude',]
+        
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
