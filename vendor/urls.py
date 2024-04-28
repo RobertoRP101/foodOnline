94c163f560b75fe2f5f35a7aeba6aef6fad8b81a
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-27 23:29:58 -0600

    Add category CRUD

--- vendor/urls.py
+++ vendor/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
    path('profile/', views.vprofile, name='vprofile'),
    path('menu-builder/', views.menu_builder, name='menu_builder'),
    path('menu-builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
+   
+   
+   # CATEGORY CRUD
+   path('menu-builder/category/add/', views.add_category, name='add_category'),
 ]
