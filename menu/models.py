50231b2c749b49018b12b6ed527c880a3c89842d
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-28 13:40:02 -0600

    Add food crud

--- menu/models.py
+++ menu/models.py
@@ -32,7 +32,7 @@ class FoodItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='foodimages')
     is_available = models.BooleanField(default=True)
-    crated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
     def __str__(self) -> str:
--- vendor/views.py
+++ vendor/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from accounts.models import  UserProfile
 from accounts.forms import UserProfileForm
-from menu.forms import CategoryForm
+from menu.forms import CategoryForm, FoodItemForm
 from menu.models import Category, FoodItem
 from .forms import VendorForm
 from .models import Vendor
@@ -115,4 +115,30 @@ def delete_category(request, pk=None):
     category.delete()
     messages.success(request, 'Category has been delete successfully!')
     return redirect('menu_builder')
+    
+def add_food(request):
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            foodtitle = form.cleaned_data['food_title']
+            food =  form.save(commit=False)
+            food.vendor = get_vendor(request)
+            food.slug = slugify(foodtitle)
+            form.save()
+            messages.success(request, 'Category added successfully!')
+            return redirect('fooditems_by_category', food.category.id)
+        else:
+            print(form.errors)
+    else:
+        form = FoodItemForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'vendor/add_food.html', context)
+
+def edit_food(request, pk=None):
+    return render(request, 'vendor/edit_food.html')
+
+def delete_food(request, pk=None):
+    return redirect('menu-builder')
     
