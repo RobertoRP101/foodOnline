f4f19c20bc3b7a10344fe74c62f015a5d4c2e05b
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-29 21:44:34 -0600

    Edit vendor model

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from vendor.forms import VendorForm
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from django.template.defaultfilters import slugify
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -84,6 +85,8 @@ def registerVendor(request):
             user.save()
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name) + '-' + str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
