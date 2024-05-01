afb0fc287b7a729448ab9b79d89b66085b603b61
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-04-30 23:12:45 -0600

    Create cart model

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from marketplace.models import Cart
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fooditem', 'quantity', 'updated_at')
+    
+    
+    
+admin.site.register(Cart, CartAdmin)
